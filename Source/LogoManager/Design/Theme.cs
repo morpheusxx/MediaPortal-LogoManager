b9279185514a9ed79391965084310c202ce8be16
morpheusxx <morpheus_xx@team-mediaportal.com>
2017-11-18 11:11:53 +0100

    Added SkipOnlineUpdate to theme to prevent auto-updating

--- Source/LogoManager/Design/Theme.cs
+++ Source/LogoManager/Design/Theme.cs
@@ -17,6 +17,14 @@ namespace MediaPortal.LogoManager.Design
       Effects = new List<AbstractEffect>();
     }
 
+    /// <summary>
+    /// Indicates that processed logos will not be updated automatically. Although the main purpose of this LogoManager is to provide such auto-update feature, 
+    /// this option can be used for "hand created" logo themes to keep the existing pre-processed logos.
+    /// It defaults to <c>false</c>, so auto-update is enabled.
+    /// </summary>
+    [DataMember]
+    public bool SkipOnlineUpdate { get; set; }
+
     [DataMember]
     public string ThemeName { get; set; }
 
